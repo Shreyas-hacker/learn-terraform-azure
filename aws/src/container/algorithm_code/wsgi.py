4717da59e5e336865a41169e5bc09e18d223942b
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2024-10-29 19:22:46 +0800

    removed old aws code

--- aws/src/container/algorithm_code/wsgi.py
+++
@@ -1,4 +0,0 @@
-import predictor as myapp
-
-#Simple wrapper for gunicorn to find app. If want to change algorithm file, change the 'predictor' to new file
-app = myapp.app
--- aws/src/lambda_function/config_lambda.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def lambda_handler(event, context):
-    prefix_name = event['PrefixName']
-    training_job_name = prefix_name + '-' + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    return training_job_name
